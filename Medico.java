c8a7592c433e571059132c687535f905863e2065
HenriKlaus <hkfmarques@gmail.com>
2026-07-01 12:29:25 -0300

    Refactor Medico constructor and salary calculation

--- Medico.java
+++ Medico.java
@@ -4,14 +4,14 @@ public class Medico extends Funcionario {
 
 	protected int horasPlantao;
 	
-	public Medico(double salarioBase, int variavel, String nome, int id, Turno turno, int horasPlantao) {
-		super(salarioBase, variavel, nome, id, turno);
+	public Medico(double salarioBase, String nome, int id, Turno turno, int horasPlantao) {
+		super(salarioBase, nome, id, turno);
 		this.horasPlantao = horasPlantao;
 	}
 	
 	@Override
 	public double calcularSalario() {
-		return salarioBase * horasPlantao; 
+		return salarioBase + horasPlantao * 100; 
 	}
 	
 	public int getHorasPlantao() {
